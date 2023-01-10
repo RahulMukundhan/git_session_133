9ef4b152f63865016593cecabe2068df62e5b347
RahulMukundhan <rahul.mukundhan@iiitb.ac.in>
2023-01-10 22:25:39 +0530

    added q1 again

--- q1.py
+++ q1.py
@@ -1,5 +1,17 @@
-x=list(map(int,input().split()))
+x=[1,2,3,4,5,6]
+sum=0
+for i in x:
+    sum=sum+x[i]
+print(sum)    
+
+# git init- initialization
+# git status- tells what is added or not
+# git add <filename> - adds all elements
+# git commit - m "<some shit>" - message whatever u want
+
+
+"""x=list(map(int,input().split()))
 sum=0
 for i in x:
     sum = sum +x[i]
-print(sum)
+print(sum)"""
